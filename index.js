4735a26c5ac1f51f4e172b493263cdcf55f4bc7a
Aminulhoque1 <aminulkupa50@gmail.com>
2022-11-02 12:46:26 -0700

    all fineshed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
 const express= require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
+require('dotenv').config();
 const app= express();
 const port = process.env.PROT|| 5000;
 
@@ -12,12 +13,85 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8qoxdwe.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+
+async function run(){
+    try{
+
+        const serviceCollection= client.db('geniusCar').collection('services');
+        const orderCollection= client.db('geniusCar').collection('orders');
+
+        app.get('/services', async(req,res)=>{
+
+            const query={};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            console.log(services);
+            res.send(services);
+        });
+
+        app.get('/services/:id', async(req, res)=>{
+
+            const id = req.params.id;
+
+            const query= { _id: ObjectId(id) }
+
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        });
+
+
+        //orders API
+
+        app.post('/orders', async(req, res)=>{
+            const order = req.body;
+            const result= await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+        // order paite 
+
+        app.get('/orders', async(req,res)=>{
+
+           let query={};
+           if(req.query.email){
+                query = {
+                    email: req.query.email,
+                }
+           }
+            const cursor= orderCollection.find(query);
+            const result= await cursor.toArray();
+            res.send(result);
+        });
+
+        app.patch('/orders/:id', async(req,res)=>{
+
+            const id= req.params.id;
+            const status = req.body.status;
+            const query = { _id: ObjectId(id)}
+            const updateDoc ={
+                $set:{
+                    status:status
+                }
+            }
+            const result = await orderCollection.updateOne(query,updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/orders/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query= { _id: ObjectId(id)}
+            const result= await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.error(error))
 
 app.get('/',(req,res)=>{
     res.send('genius car server has running')
