befc2ee40ed46bceb07da8dd3d4d4df3c2b80d71
Aminulhoque1 <aminulkupa50@gmail.com>
2022-11-04 11:15:47 -0700

    last commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express= require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { query } = require('express');
 require('dotenv').config();
@@ -14,6 +16,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req,res,next){
+    const authHeader= req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send({message:'unauthorized access'})
+    }
+    const token= authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(401).send({message:'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run(){
     try{
@@ -21,12 +38,19 @@ async function run(){
         const serviceCollection= client.db('geniusCar').collection('services');
         const orderCollection= client.db('geniusCar').collection('orders');
 
+        app.post('/jwt',(req,res)=>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'2d'})
+            res.send({token})
+        })
+
+
         app.get('/services', async(req,res)=>{
 
             const query={};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
-            console.log(services);
+            
             res.send(services);
         });
 
@@ -44,14 +68,19 @@ async function run(){
         //orders API
 
         app.post('/orders', async(req, res)=>{
-            const order = req.body;
+             const order = req.body;
             const result= await orderCollection.insertOne(order);
             res.send(result);
         })
 
         // order paite 
 
-        app.get('/orders', async(req,res)=>{
+        app.get('/orders', verifyJWT, async(req,res)=>{
+             const decoded= req.decoded;
+             
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message:'unauthorized access'})
+            }
 
            let query={};
            if(req.query.email){
